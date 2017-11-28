71e59f7a14c66b87cdc819cfa190ebcb449e9438
lodeane <lodeanekelly@gmail.com>
2017-11-27 19:23:22 -0500

    synced untracked files

---
+++ Models/Repositories/UserTypeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SS.Models.Repositories
+{
+    public class UserTypeRepository : Repository<UserType>, IUserTypeRepository
+    {
+        public EasyFindPropertiesEntities EasyFindPropertiesEntities
+        {
+            get { return (EasyFindPropertiesEntities)dbCtx; }
+        }
+
+        public UserTypeRepository(EasyFindPropertiesEntities dbctx)
+            : base(dbctx)
+        { }
+    }
+}
